b3988d0853450b983ee39b21becbf9fa4e7f2d6c
david <davidnzogu5@gmail.com>
2025-09-30 02:01:17 +1000

    Add prac 1 files

---
+++ prac_01/shop_calculator.py
@@ -0,0 +1,14 @@
+"""
+Pattern menu of a menu driven program
+"""
+number_of_items = int(input("Number of items: "))
+total_price = 0.0
+
+for _ in range(number_of_items):
+    price = float(input("Price of item: "))
+    total_price += price
+
+if total_price > 100:
+    total_price *= 0.9  # Apply 10% discount
+
+print(f"Total price for {number_of_items} items is ${total_price:.2f}")
