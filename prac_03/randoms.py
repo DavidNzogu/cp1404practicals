e0416a79c27f40940a2019afb072f3494757f12f
david <davidnzogu5@gmail.com>
2025-10-07 22:42:37 +1000

    Add basic demonstration of random.randint, random.randrange, and random.uniform usage with output ranges

--- prac_03/randoms.py
+++ prac_03/randoms.py
@@ -6,7 +6,7 @@ print(random.randint(5, 20))  # line 1
 print(random.randrange(3, 10, 2))  # line 2
 # Smallest number was 3
 # Largest number was 9
-# Throughout the multiple runs line 2 never produced 4
+# Throughout the multiple runs, line 2 never produced 4
 print(random.uniform(2.5, 5.5))  # line 3
 # Smallest number was 2.8731295122839287
 # largest number was 5.195020951333553
